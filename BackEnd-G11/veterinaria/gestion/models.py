ab91b94eb92854815fc808df98fe801290fb2203
Edison <epescoran@live.com>
2023-04-03 14:59:10 -0500

    uso de cloudinary

--- BackEnd-G11/veterinaria/gestion/models.py
+++ BackEnd-G11/veterinaria/gestion/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
-
+from cloudinary.models import CloudinaryField
 
 class ManejoUsuario(BaseUserManager):
     def create_superuser(self, correo, nombre, apellido, password, tipoUsuario):
@@ -29,6 +29,21 @@ class Usuario(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD='correo'
 
     REQUIRED_FIELDS = ['nombre','apellido','tipoUsuario']
-    object= ManejoUsuario()
+    
+    objects= ManejoUsuario()
     class Meta:
         db_table='usuarios'
+
+
+class Mascota(models.Model):
+    id=models.AutoField(primary_key=True, null=False)
+    nombre =models.TextField(null=False)
+    sexo=models.TextField(choices=[('HEMBRA','HEMBRA'),('MACHO','MACHO')])
+    fechaNacimiento=models.DateField(db_column='Fecha_nacimiento')
+    alergias= models.TextField()
+    foto= CloudinaryField('foto')
+    
+    cliente = models.ForeignKey(to=Usuario,  on_delete=models.RESTRICT, db_column='clinete_id')
+
+    class Meta:
+        db_table='mascota'
---
+++ BackEnd-G11/veterinaria/gestion/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import RegistroUsuario
+
+
+urlpatterns =[
+    path('registro', RegistroUsuario.as_view())
+]
